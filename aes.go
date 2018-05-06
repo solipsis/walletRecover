31895fbfaf70ab7f97e5af5206915bf76d740c75
solipsis <david@keepkey.com>
2018-05-06 14:41:30 -0600

    add legacy wallet types and cleanup

--- aes.go
+++ aes.go
@@ -41,9 +41,12 @@ func main() {
 	defer f.Close()
 	sc := bufio.NewScanner(f)
 
-	//
 	worklist := make(chan string)
-	results := make(chan string)
+	progress := make(chan string)
+	errors := make(chan error)
+	done := make(chan string)
+
+	// seed the worklist using the passwords from the dictionary file
 	go func(sc *bufio.Scanner) {
 		defer close(worklist)
 		for sc.Scan() {
@@ -51,145 +54,126 @@ func main() {
 		}
 	}(sc)
 
+	// 20 worker goroutines pulling from the worklist
 	wg := new(sync.WaitGroup)
-
 	for x := 0; x < 20; x++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			for p := range worklist {
-				//cipherKey := pbkdf2.Key([]byte(p), []byte{}, 10, 32, sha1.New)
-				dec, err := decrypt(p, cipherText)
-				if err != nil {
-					log.Fatal(err)
+			for {
+				select {
+				case p := <-worklist:
+					dec, err := decrypt(p, cipherText)
+					if err != nil {
+						errors <- err
+					}
+
+					var e interface{}
+					err = json.Unmarshal([]byte(dec), &e)
+					if err != nil {
+						progress <- fmt.Sprintf("Not json: %s", p)
+						continue
+					}
+					close(done)
+					fmt.Printf("Decoded: %s\n", string(dec))
+					fmt.Println("Done")
+					fmt.Println("************")
+					fmt.Println("Success: " + p)
+
+				case <-done:
+					return
 				}
-				var e interface{}
-				err = json.Unmarshal([]byte(dec), &e)
-				if err != nil {
-					results <- fmt.Sprintf("Not json: %s", p)
-					continue
-				}
-				fmt.Printf("Decoded: %s\n", string(dec))
-				fmt.Println("Done")
-				fmt.Println("************")
-				fmt.Println("Success: " + p)
-				results <- fmt.Sprintf("Success: %s", p)
-				panic("Yay")
 			}
 		}()
 	}
 
+	// Occasionally print intermediate results
 	go func() {
 		x := 0
-		for r := range results {
+		for update := range progress {
 			if x%10000 == 0 {
-				fmt.Printf("%s %d\n", r, x)
+				fmt.Printf("Health Check: %s | %d passwords tried so far\n", update, x)
 			}
 			x++
+		}
+	}()
+
+	// Gracefully shutdown if an error occurs
+	go func() {
+		for err := range errors {
+			fmt.Println("Error: ", err)
+			close(done)
+			close(errors)
 		}
 	}()
 
 	wg.Wait()
 }
 
-/*
-func main() {
-	js := `{"Id":4, "Potato":"Tomato"}`
-	cipherKey := pbkdf2.Key([]byte("stack901"), []byte{}, 10, 32, sha1.New)
-	enc, err := encrypt([]byte(cipherKey), js)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Encrypted: %s\n", string(enc))
-	dec, err := decrypt(cipherKey, enc)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var e interface{}
-	err = json.Unmarshal([]byte(dec), &e)
-	if err != nil {
-		fmt.Println("NOT JSON", err)
-	}
-	fmt.Printf("Decoded: %s\n", string(dec))
-	fmt.Println("Done")
-
-}
-*/
-
+// most recent legacy wallet format
 func decrypt(plainKey string, cipherText []byte) (string, error) {
 
 	// iv and salt are the first 16 bytes
 	iv := cipherText[:aes.BlockSize]
 	salt := cipherText[:aes.BlockSize]
 	cipherText = cipherText[aes.BlockSize:]
+
 	cipherKey := pbkdf2.Key([]byte(plainKey), salt, 10, 32, sha1.New)
 	block, err := aes.NewCipher(cipherKey)
 	if err != nil {
 		return "", err
 	}
-	ecb := cipher.NewCBCDecrypter(block, iv)
 
+	ecb := cipher.NewCBCDecrypter(block, iv)
 	decrypted := make([]byte, len(cipherText))
 	ecb.CryptBlocks(decrypted, cipherText)
 
 	return string(unpad(decrypted)), nil
 }
 
-/*
-// no salt
-func decrypt(plainKey string, encrypted string) (string, error) {
-
-	if len(cipherText) < aes.BlockSize {
-		return "", errors.New("Ciphertext block size is too short!")
-	}
+// Only 1 iteration for pbkdf2
+func decryptLegacy1(plainKey string, cipherText []byte) (string, error) {
 
 	// iv and salt are the first 16 bytes
 	iv := cipherText[:aes.BlockSize]
-	salt := []byte{}
+	salt := cipherText[:aes.BlockSize]
 	cipherText = cipherText[aes.BlockSize:]
-	cipherKey := pbkdf2.Key([]byte(plainKey), salt, 10, 32, sha1.New)
+
+	cipherKey := pbkdf2.Key([]byte(plainKey), salt, 1, 32, sha1.New)
 	block, err := aes.NewCipher(cipherKey)
 	if err != nil {
 		return "", err
 	}
-	ecb := cipher.NewCBCDecrypter(block, iv)
 
+	ecb := cipher.NewCBCDecrypter(block, iv)
 	decrypted := make([]byte, len(cipherText))
 	ecb.CryptBlocks(decrypted, cipherText)
 
 	return string(unpad(decrypted)), nil
 }
-*/
 
+// No Salt or IV
+func decryptLegacy2(plainKey string, cipherText []byte) (string, error) {
+
+	iv := []byte{}
+	salt := []byte{}
+
+	cipherKey := pbkdf2.Key([]byte(plainKey), salt, 10, 32, sha1.New)
+	block, err := aes.NewCipher(cipherKey)
+	if err != nil {
+		return "", err
+	}
+
+	ecb := cipher.NewCBCDecrypter(block, iv)
+	decrypted := make([]byte, len(cipherText))
+	ecb.CryptBlocks(decrypted, cipherText)
+
+	return string(unpad(decrypted)), nil
+}
+
+// Remove ISO 10126 padding
 func unpad(src []byte) []byte {
 	length := len(src)
 	unpadding := int(src[length-1])
 	return src[:(length - unpadding)]
 }
-
-/*
-func encrypt(key []byte, message string) (encmess string, err error) {
-	plainText := []byte(message)
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return
-	}
-
-	//IV needs to be unique, but doesn't have to be secure.
-	//It's common to put it at the beginning of the ciphertext.
-	cipherText := make([]byte, aes.BlockSize+len(plainText))
-	iv := cipherText[:aes.BlockSize]
-	if _, err = io.ReadFull(rand.Reader, iv); err != nil {
-		return
-	}
-
-	//stream := cipher.NewCFBEncrypter(block, iv)
-	//stream := cipher.NewCBCEncrypter(block, iv)
-	//stream.XORKeyStream(cipherText[aes.BlockSize:], plainText)
-
-	//returns to base64 encoded string
-	encmess = base64.StdEncoding.EncodeToString(cipherText)
-	return
-}
-*/
